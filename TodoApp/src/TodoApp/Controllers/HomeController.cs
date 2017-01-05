c80d0a35ce591c6885b9e4a020e3b2c439e367de
Petar Kovacevic <petar.kovacevic@outlook.com>
2017-01-05 13:01:29 +0100

    initialized todo app project

---
+++ TodoApp/src/TodoApp/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace TodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemoryCache _cache;
+
+        public HomeController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult SessionTest()
+        {
+
+            const string counterKey = "MyCounter";
+
+            // Get counter from session 
+            var counter = HttpContext.Session.GetInt32(counterKey);
+
+            // Initialize or increment counter
+            if (counter == null)
+            {
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+
+            // Save incremented value back to session
+            HttpContext.Session.SetInt32(counterKey, counter.Value);
+
+            return View(counter);
+        }
+
+        public async Task<IActionResult> CacheTest()
+        {
+            const string cacheKey = "That_hard_to_get_string";
+            var hardToGet = _cache.Get(cacheKey) as string;
+            if (hardToGet == null)
+            {
+                // not in cache - get it then...
+                hardToGet = await GetThatHardToGetStringAsync();
+                // save in cache for future use
+                _cache.Set(cacheKey, hardToGet);
+            }
+
+            return View("CacheTest",hardToGet);
+        }
+
+        private async Task<string> GetThatHardToGetStringAsync()
+        {
+            await Task.Delay(5000);
+            return ":)";
+        }
+    }
+}
---
+++ TodoApp/src/TodoApp/Interfaces/ITodoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Models;
+
+namespace TodoApp.Interfaces
+{
+    public interface ITodoRepository
+    {
+        TodoItem Get(Guid todoId, Guid userId);
+        void Add(TodoItem todoItem);
+        bool Remove(Guid todoId, Guid userId);
+        void Update(TodoItem todoItem, Guid userId);
+        bool MarkAsCompleted(Guid todoId, Guid userId);
+        List<TodoItem> GetAll(Guid userId);
+        List<TodoItem> GetActive(Guid userId);
+        List<TodoItem> GetCompleted(Guid userId);
+        List<TodoItem> GetFiltered(Func<TodoItem, bool> filterFunction, Guid userId);
+        List<TodoItem> GetLastTodos(int n);
+
+    }
+}
